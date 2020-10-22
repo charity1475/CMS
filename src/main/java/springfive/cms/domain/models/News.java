e73a2c3e83856207639b88ef2dfd552a9d193a5e
Steinny <charitymbisi@gmail.com>
2020-10-22 13:01:53 +0300

    models

--- src/main/java/springfive/cms/domain/models/News.java
+++ src/main/java/springfive/cms/domain/models/News.java
@@ -1,4 +1,16 @@
 package springfive.cms.domain.models;
 
+import java.util.Set;
+import lombok.Data;
+
+@Data
 public class News {
+    String id;
+    String title;
+    String content;
+    User author;
+    Set<User> mandatoryReviewers;
+    Set<Review> reviewers;
+    Set<Category> categories;
+    Set<Tag> tags;
 }
---
+++ src/main/java/springfive/cms/domain/models/Review.java
@@ -0,0 +1,13 @@
+package springfive.cms.domain.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+    String userId;
+    String status;
+}
