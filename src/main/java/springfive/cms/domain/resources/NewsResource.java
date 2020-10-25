5082958a35eeb114e96030fefb14a23038d68a07
Steinny <charitymbisi@gmail.com>
2020-10-25 21:13:55 +0300

    news resource

--- src/main/java/springfive/cms/domain/resources/NewsResource.java
+++ src/main/java/springfive/cms/domain/resources/NewsResource.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import springfive.cms.domain.models.News;
+import springfive.cms.domain.models.Review;
 import springfive.cms.domain.models.User;
 import springfive.cms.domain.vo.CategoryRequest;
 import springfive.cms.domain.vo.NewsRequest;
@@ -34,5 +35,13 @@ public class NewsResource {
     @PutMapping("/{id}")
     public ResponseEntity<News> updateNews( @PathVariable("id") String id, NewsRequest news){
         return new ResponseEntity<>(new News(), HttpStatus.OK);
+    }
+    @GetMapping(value = "/{id}/review/{userId}")
+    public ResponseEntity<Review> review(@PathVariable("id") String id, @PathVariable("userId") String userId){
+        return ResponseEntity.ok(new Review());
+    }
+    @GetMapping(value = "/revised")
+    public ResponseEntity<List<News>> revisedNews(){
+        return ResponseEntity.ok(Arrays.asList(new News(), new News()));
     }
 }
