913f436715fd953acbf9ddc54215b31aae1b9d5d
Steinny <charitymbisi@gmail.com>
2020-10-25 21:03:27 +0300

    controller for news

--- src/main/java/springfive/cms/domain/vo/NewsRequest.java
+++ src/main/java/springfive/cms/domain/vo/NewsRequest.java
@@ -1,4 +1,15 @@
 package springfive.cms.domain.vo;
 
+import lombok.Data;
+import springfive.cms.domain.models.Category;
+import springfive.cms.domain.models.Tag;
+
+import java.util.Set;
+
+@Data
 public class NewsRequest {
+    String title;
+    String content;
+    Set<Category> categories;
+    Set<Tag> tags;
 }
