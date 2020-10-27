f8526291a0afaff4aba4985eb2b703d9fcb0906b
Steinny <charitymbisi@gmail.com>
2020-10-27 09:06:18 +0300

    swagger API doc

--- src/main/java/springfive/cms/domain/services/NewsService.java
+++ src/main/java/springfive/cms/domain/services/NewsService.java
@@ -1,2 +1,8 @@
-package springfive.cms.domain.services;public class NewsService {
+package springfive.cms.domain.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NewsService {
+
 }
