6d8ce3c0764809dc6309985508c5d2c9ca9217c6
Steinny <charitymbisi@gmail.com>
2020-10-30 00:46:22 +0300

    AngularJS module

---
+++ src/main/resources/static/app/components/categories/category-controller.js
@@ -0,0 +1,44 @@
+(function (angular) {
+    'use strict';
+    /*controllers*/
+    angular.module('cms.modules.category.controllers', []).
+        controller(
+            'CategoryCreateController',
+        [
+            '$scope','CategoryService','$state',
+            function ($scope,CategoryService,$state) {
+                $scope.resetForm = function () {
+                    $scope.category = null;
+                };
+                $scope.create = function (category) {
+                    CategoryService.create(category).then(
+                        function (data) {
+                            console.log("success on creating category!")
+                            $state.go('categories')
+                        },
+                        function (err) {
+                            console.log("Error on creating category")
+                        }
+                    );
+                };
+            }
+        ]
+    ).
+        controller(
+            'CategoryListController',
+        [
+           '$scope','CategoryService',
+            function ($scope, CategoryService) {
+                CategoryService.find().then(
+                    function (data) {
+                        $scope.categories = data.data;
+                    },
+                    function (err) {
+                        console.log(err);
+                    }
+                );
+            }
+        ]
+    );
+    
+})(angular);
