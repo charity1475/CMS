58e2e0dedbadb0f46e085715a168753e0a2eef8d
Steinny <charitymbisi@gmail.com>
2020-11-09 00:21:20 +0300

    category persistance

--- src/main/java/springfive/cms/domain/models/Category.java
+++ src/main/java/springfive/cms/domain/models/Category.java
@@ -1,8 +1,25 @@
 package springfive.cms.domain.models;
 
+import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
 @Data
+@Document(collection = "categories")
 public class Category {
+
+    @Id
     String id;
     String name;
+
+    @Builder
+    public static Category newCategory(String name){
+        final  Category category = new Category();
+        category.setId(UUID.randomUUID().toString());
+        category.setName(name);
+        return category;
+    }
 }
