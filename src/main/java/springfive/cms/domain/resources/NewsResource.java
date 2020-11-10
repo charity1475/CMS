f14f02e7970508b0f86376ce2c44cccf809cb5b9
Steinny <charitymbisi@gmail.com>
2020-11-11 01:21:02 +0300

    API configured

--- src/main/java/springfive/cms/domain/resources/NewsResource.java
+++ src/main/java/springfive/cms/domain/resources/NewsResource.java
@@ -2,9 +2,11 @@ package springfive.cms.domain.resources;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import springfive.cms.domain.models.News;
 import springfive.cms.domain.models.Review;
+import springfive.cms.domain.services.NewsService;
 import springfive.cms.domain.vo.NewsRequest;
 
 import java.util.Arrays;
@@ -13,26 +15,36 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/news")
 public class NewsResource {
+
+    private final NewsService newsService;
+
+    public NewsResource(NewsService newsService) {
+        this.newsService = newsService;
+    }
+
     @GetMapping(value = "/{id}")
-    public ResponseEntity<News> findOne(@PathVariable("id") String id){
-        return ResponseEntity.ok( new News());
+    public ResponseEntity<Object> findOne(@PathVariable("id") String id){
+        return ResponseEntity.ok(this.newsService.findOne(id));
     }
     @GetMapping
-    public ResponseEntity<List<News>> findAll(){
-        return ResponseEntity.ok(Arrays.asList(new News(), new News()));
+    public ResponseEntity<Object> findAll(){
+        return ResponseEntity.ok(this.newsService.findAll());
     }
     @PostMapping
-    public ResponseEntity<News> newNews(NewsResource news){
-        return new ResponseEntity<>( new News(), HttpStatus.CREATED);
+    public ResponseEntity<News> newNews(NewsRequest news){
+        return new ResponseEntity<>((MultiValueMap<String, String>) this.newsService.create(news),
+                HttpStatus.CREATED);
     }
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeNews (@PathVariable("id") String id){
-
+        this.newsService.delete(id);
     }
     @PutMapping("/{id}")
     public ResponseEntity<News> updateNews( @PathVariable("id") String id, NewsRequest news){
-        return new ResponseEntity<>(new News(), HttpStatus.OK);
+        return new
+                ResponseEntity<>((MultiValueMap<String, String>) this.newsService.update(id,news),
+                HttpStatus.OK);
     }
     @GetMapping(value = "/{id}/review/{userId}")
     public ResponseEntity<Review> review(@PathVariable("id") String id, @PathVariable("userId") String userId){
@@ -40,6 +52,6 @@ public class NewsResource {
     }
     @GetMapping(value = "/revised")
     public ResponseEntity<List<News>> revisedNews(){
-        return ResponseEntity.ok(Arrays.asList(new News(), new News()));
+        return ResponseEntity.ok(Arrays.asList(new News()));
     }
 }
--- src/main/java/springfive/cms/domain/services/NewsService.java
+++ src/main/java/springfive/cms/domain/services/NewsService.java
@@ -1,7 +1,11 @@
 package springfive.cms.domain.services;
 
 import org.springframework.stereotype.Service;
+import springfive.cms.domain.models.News;
 import springfive.cms.domain.repository.NewsRepository;
+import springfive.cms.domain.vo.NewsRequest;
+
+import java.util.UUID;
 
 @Service
 public class NewsService {
@@ -9,5 +13,34 @@ public class NewsService {
 
     public NewsService(NewsRepository newsRepository) {
         this.newsRepository = newsRepository;
+    }
+
+    public Object findOne(String id) {
+        return this.newsRepository.findById(id);
+    }
+
+    public Object findAll() {
+        return this.newsRepository.findAll();
+    }
+
+    public Object create(NewsRequest newsRequest) {
+        News user = new News();
+        user.setId(UUID.randomUUID().toString());
+        user.setTitle(newsRequest.getTitle());
+        user.setContent(newsRequest.getContent());
+        return this.newsRepository.save(user);
+    }
+
+    public void delete(String id) {
+        this.newsRepository.deleteById(id);
+    }
+
+    public Object update(String id, NewsRequest newsRequest) {
+        return this.newsRepository.findById(id).flatMap(user -> {
+            user.setTitle(newsRequest.getTitle());
+            user.setContent(newsRequest.getContent());
+            user.setCategories(newsRequest.getCategories());
+            return this.newsRepository.save(user);
+        });
     }
 }
